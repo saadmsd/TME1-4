6c57fb5c8498cf48ba968d460060d2a7f9d91a4b
saadmsd <saadmousstaid@gmail.com>
2023-01-31 13:09:30 +0100

    Modifications q1-q2

--- TME1-4/q1.py
+++ TME1-4/q1.py
@@ -1,10 +1,8 @@
 def lectureFichierEtu(s): # Definition d'une fonction, avec un parametre (s). Ne pas oublier les ":"
     monFichier = open(s, "r") # Ouverture en lecture. Indentation par rapport a la ligne d'avant (<-> bloc).
     contenu = monFichier.readlines() # Contenu contient une liste de chainces de caracteres, chaque chaine correspond a une ligne       
-    nbetu=contenu[0].split()  # ligne.split() renvoie une liste de toutes les chaines contenues dans la chaine ligne (separateur=espace)
-    for i in range(0,int(nbetu[0])): #Boucle i variant de 0 a contenue[0]
+    for i in range(len(contenu)): #Boucle i variant de 0 a contenue[0]
         contenu[i]=contenu[i].split()
-    contenu=contenu[0:int(nbetu[0])] #On ne garde que les lignes contenant les preferences
     monFichier.close() #Fermeture du fichier
     return contenu
 
@@ -12,10 +10,8 @@ def lectureFichierEtu(s): # Definition d'une fonction, avec un parametre (s). Ne
 def lectureFichierSpe(s): # Definition d'une fonction, avec un parametre (s). Ne pas oublier les ":"
     monFichier = open(s, "r") # Ouverture en lecture. Indentation par rapport a la ligne d'avant (<-> bloc).
     contenu = monFichier.readlines() # Contenu contient une liste de chainces de caracteres, chaque chaine correspond a une ligne       
-    nbetu=contenu[0].split(" ")  # ligne.split() renvoie une liste de toutes les chaines contenues dans la chaine ligne (separateur=espace)
-    for i in range(0,9): #Boucle i variant de 0 a contenue[0]
-        contenu[i]=contenu[i+1].split()
-    contenu=contenu[0:9] #On ne garde que les lignes contenant les preferences
+    for i in range(len(contenu)): #Boucle i variant de 0 a contenue[0]
+        contenu[i]=contenu[i].split()
     monFichier.close() #Fermeture du fichier
     return contenu
 
@@ -23,27 +19,29 @@ def GS(etu,spe):
     etu=lectureFichierEtu(etu)
     spe=lectureFichierSpe(spe)
     nb = int(etu[0][0])
-    mariage=[[]]
+    mariage=[]
     while len(mariage) < nb:  #tant que il existe un etudiant libre
-        for i in range(1,nb):   #pour chaque etudiant
-            if etu[i][1] not in mariage[i-1]:    #si l'etudiant est libre
+        for i in range(1,nb+1):   #pour chaque etudiant
+            marie = False
+            for couple in mariage: #on verifie si il est deja marie
+                if etu[i][1] in couple:
+                    marie = True
+                    break
+            if not marie:  #si il est libre
                 mariage.append([etu[i][1]])     #on lajoute a la liste des maries
                 for j in range(2,len(etu[i])):  #pour chaque preference de l'etudiant
-                    cap = int(spe[0][int(etu[i][j])+1])  #on recupere la capacite de la specialite
+                    cap = int(spe[1][int(etu[i][j])+1])  #on recupere la capacite de la specialite
                     for k in range(0,len(mariage)):     #on parcourt la liste des maries
                         if etu[i][j] in mariage[k]:      #si la spe est presente dans la liste des maries
                             cap = cap - 1
                     if cap > 0:     #si la specialite n'est pas pleine
-                        mariage[i].append(etu[i][j])  #on ajoute la spe a l'etudiant
+                        mariage[i-1].append(etu[i][j])  #on ajoute la spe a l'etudiant
                         break
-                    """else:
-                        for l in range(2,len)):
-                            if etu[i][j] in mariage[l]:
-                                mariage.remove(mariage[l])
-                                mariage[i].append(etu[i][j])
-                                break"""
-    mariage.remove(mariage[0])
+                    #else:
+                        #for s in reversed(spe[j]):  #on parcourt la liste des preferences de la specialite
+                            #if etu[s+1][1] in mariage[i]:
+                                #break
+                            
+    #mariage.remove(mariage[0])
     return mariage
-
-
 print(GS("PrefEtu.txt","PrefSpe.txt"))
